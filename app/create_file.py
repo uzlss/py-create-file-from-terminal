20240b5afe98ca53260b56c5a4c258bb3d5fe9e2
Ihor Bachuk <igor.bachuk04@gmail.com>
2024-09-24 19:27:41 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,40 @@
-# write your code here
+import datetime
+import sys
+import os
+
+
+def create_file(path: str) -> None:
+    with open(path, "a") as file:
+        line = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        index = 0
+
+        while line != "stop":
+            if index:
+                line = f"{index} {line}"
+
+            file.write(line + "\n")
+            line = input("Enter content line: ")
+            index += 1
+
+        file.write("\n")
+
+
+def read_line() -> None:
+    line_args = sys.argv[1:]
+    path = []
+
+    if "-d" in line_args:
+        for index, arg in enumerate(line_args[1:]):
+            if arg == "-f":
+                line_args = line_args[index + 1:]
+                break
+            path.append(arg)
+
+        os.makedirs(os.path.join(*path), exist_ok=True)
+
+    if "-f" in line_args:
+        create_file(os.path.join(*path, line_args[-1]))
+
+
+if __name__ == "__main__":
+    read_line()
